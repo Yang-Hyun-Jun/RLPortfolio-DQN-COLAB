7f5c277fd4abab82e3fa1f12cfd71bd7448c8acd
Hyun Jun <eppioes@gmail.com>
2022-05-24 12:14:30 +0900

    for GPU

--- Test.py
+++ Test.py
@@ -1,4 +1,3 @@
-import DataManager
 import Visualizer
 import utils
 import torch
@@ -10,112 +9,94 @@ from Agent import agent
 from Q_network import Score
 from Q_network import Qnet
 
-if __name__ == "__main__":
-    stock_code = ["010140", "005930", "034220"]
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    path_list = []
-    for code in stock_code:
-        path = utils.Base_DIR + "/" + code
-        path_list.append(path)
+class DQNTester:
+    def __init__(self, test_data, balance, min_trading_price, max_trading_price, K):
+        self.test_data = test_data
 
-    #test data load
-    train_data, test_data = DataManager.get_data_tensor(path_list,
-                                                        train_date_start="20090101",
-                                                        train_date_end="20150101",
-                                                        test_date_start="20170102",
-                                                        test_date_end=None)
+        self.state1_dim = 5
+        self.state2_dim = 2
+        self.K = K
 
-    #dimension
-    K = len(stock_code)
+        self.score_net = Score().to(device)
+        self.qnet = Qnet(self.score_net).to(device)
+        self.qnet_target = Qnet(self.score_net, self.K)
 
-    #Test Model load
-    score_net = Score()
-    qnet = Qnet(score_net, K)
-    qnet_target = Qnet(score_net, K)
-    # qnet = Qnet(state2_dim=K+1, K=K)
-    # qnet_target = Qnet(state2_dim=K+1, K=K)
+        self.balance = balance
+        self.min_trading_price = min_trading_price
+        self.max_trading_price = max_trading_price
 
-    balance = 15000000
-    min_trading_price = 0
-    max_trading_price = 500000
+        self.env = environment(chart_data=test_data)
+        self.agent = agent(environment=self.env,
+                           qnet=self.qnet, qnet_target=self.qnet_target, K=self.K,
+                           lr=0.0, tau=0.0, discount_factor=0.0,
+                           min_trading_price=self.min_trading_price,
+                           max_trading_price=self.max_trading_price)
 
-    #Agent
-    environment = environment(chart_data=test_data)
-    agent = agent(environment=environment,
-                  qnet=qnet, K=K,
-                  qnet_target=qnet_target,
-                  lr = 1e-4, tau = 0.005, discount_factor=0.9,
-                  min_trading_price=min_trading_price,
-                  max_trading_price=max_trading_price)
+        model_path = utils.SAVE_DIR + "/Models" + "/DQNPortfolio.pth"
+        self.agent.epsilon = 0
+        self.agent.qnet.load_state_dict(torch.load(model_path))
+        self.agent.qnet_target.load_state_dict(agent.qnet.state_dict())
 
-    agent.epsilon = 0.0
-    #Model parameter load
-    model_path = utils.SAVE_DIR + "/DQNPortfolio/Models" + "/DQNPortfolio.pth"
-    agent.qnet.load_state_dict(torch.load(model_path))
-    agent.qnet_target.load_state_dict(agent.qnet.state_dict())
+    def run(self):
+        metrics = Metrics()
+        self.agent.set_balance(self.balance)
+        self.agent.reset()
+        self.agent.environment.reset()
+        state1 = self.agent.environment.observe()
+        state2 = self.agent.portfolio
+        steps_done = 0
 
-    #Model Test
-    metrics = Metrics()
-    agent.set_balance(balance)
-    agent.reset()
-    agent.environment.reset()
-    agent.epsilon = 0
-    state1 = agent.environment.observe()
-    state2 = agent.portfolio
-    steps_done = 0
+        while True:
+            index, actions, confidences = \
+                self.agent.get_action(torch.tensor(state1, device=device).float().view(1, K, -1),
+                                      torch.tensor(state2, device=device).float().view(1, K + 1))
 
-    while True:
-        index, actions, confidences = agent.get_action(torch.tensor(state1).float().view(1, K, -1),
-                                                       torch.tensor(state2).float().view(1, K+1))
+            _, next_state1, next_state2, reward, done = self.agent.step(actions, confidences)
 
-        _, next_state1, next_state2, reward, done = agent.step(actions, confidences)
+            state1 = next_state1
+            state2 = next_state2
 
-        state1 = next_state1
-        state2 = next_state2
+            metrics.portfolio_values.append(self.agent.portfolio_value)
+            metrics.profitlosses.append(self.agent.profitloss)
+            metrics.balances.append(self.agent.balance)
+            if steps_done % 1 == 0:
+                print(f"balance:{self.agent.balance}")
+                print(f"stocks:{self.agent.num_stocks}")
+                print(f"actions:{actions}")
+            if done:
+                print(f"model{self.agent.profitloss}")
+                break
 
-        metrics.portfolio_values.append(agent.portfolio_value)
-        metrics.profitlosses.append(agent.profitloss)
-        metrics.balances.append(agent.balance)
-        if steps_done % 50 == 0:
-            print(f"balance:{agent.balance}")
-            print(f"stocks:{agent.num_stocks}")
-            # print(f"actions:{actions}")
-        if done:
-            print(f"model{agent.profitloss}")
-            break
+        # Benchmark: B&H
+        self.agent.set_balance(self.balance)
+        self.agent.reset()
+        self.agent.environment.reset()
+        self.agent.environment.observe()
+        while True:
+            action = np.zeros(self.K)
+            confidence = np.ones(self.K)
 
+            _, next_state1, next_state2, reward, done = self.agent.step(action, confidence)
+            metrics.profitlosses_BH.append(self.agent.profitloss)
 
-    #Benchmark: B&H
-    agent.set_balance(balance)
-    agent.reset()
-    agent.environment.reset()
-    agent.epsilon = 0
-    state1 = agent.environment.observe()
-    state2 = agent.portfolio
-    while True:
-        action = np.zeros(K)
-        confidence = np.ones(K)
-        _, next_state1, next_state2, reward, done = agent.step(action, confidence)
+            if done:
+                print(f"B&H{self.agent.profitloss}")
+                break
 
-        state1 = next_state1
-        state2 = next_state2
-        metrics.profitlosses_BH.append(agent.profitloss)
-        if done:
-            print(f"B&H{agent.profitloss}")
-            break
+        Vsave_path2 = utils.SAVE_DIR + "/DQNPortfolio" + "/Metrics" + "/Portfolio Value Curve_test"
+        Vsave_path4 = utils.SAVE_DIR + "/DQNPortfolio" + "/Metrics" + "/Profitloss Curve_test"
+        Msave_path1 = utils.SAVE_DIR + "/DQNPortfolio" + "/Metrics" + "/Portfolio Value_test"
+        Msave_path2 = utils.SAVE_DIR + "/DQNPortfolio" + "/Metrics" + "/Profitloss_test"
+        Msave_path3 = utils.SAVE_DIR + "/DQNPortfolio" + "/Metrics" + "/Profitloss B&H"
+        Msave_path4 = utils.SAVE_DIR + "/DQNPortfolio" + "/Metrics" + "/Balances"
 
-    Vsave_path2 = utils.SAVE_DIR + "/DQNPortfolio" + "/Metrics" + "/Portfolio Value Curve_test"
-    Vsave_path4 = utils.SAVE_DIR + "/DQNPortfolio" + "/Metrics" + "/Profitloss Curve_test"
-    Msave_path1 = utils.SAVE_DIR + "/DQNPortfolio" + "/Metrics" + "/Portfolio Value_test"
-    Msave_path2 = utils.SAVE_DIR + "/DQNPortfolio" + "/Metrics" + "/Profitloss_test"
-    Msave_path3 = utils.SAVE_DIR + "/DQNPortfolio" + "/Metrics" + "/Profitloss B&H"
-    Msave_path4 = utils.SAVE_DIR + "/DQNPortfolio" + "/Metrics" + "/Balances"
+        metrics.get_portfolio_values(save_path=Msave_path1)
+        metrics.get_profitlosses(save_path=Msave_path2)
+        metrics.get_profitlosses_BH(save_path=Msave_path3)
+        metrics.get_balances(save_path=Msave_path4)
 
-    metrics.get_portfolio_values(save_path=Msave_path1)
-    metrics.get_profitlosses(save_path=Msave_path2)
-    metrics.get_profitlosses_BH(save_path=Msave_path3)
-    metrics.get_balances(save_path=Msave_path4)
-
-    Visualizer.get_portfolio_value_curve(metrics.portfolio_values, save_path=Vsave_path2)
-    Visualizer.get_profitloss_curve(metrics.profitlosses, metrics.profitlosses_BH, save_path=Vsave_path4)
+        Visualizer.get_portfolio_value_curve(metrics.portfolio_values, save_path=Vsave_path2)
+        Visualizer.get_profitloss_curve(metrics.profitlosses, metrics.profitlosses_BH, save_path=Vsave_path4)
 
